d6ec4a2bdce09a6357a2a304524b57054801bec6
Jim Gao <jim@jimgao.tk>
2019-03-09 06:20:20 -0500

    Changed API endpoints

--- server.py
+++ server.py
@@ -4,35 +4,47 @@ from contracts import DCCInterface
 import json
 import _thread
 import time
+import traceback
 
 app = Flask(__name__)
 jobs = []
+jobs_details = []
 
 def thread_prune_entries():
     global jobs
+    global jobs_details
     while True:
         print ("Pruning entires!")
         new_jobs = []
+        new_jobs_details = []
         for j in jobs:
             try:
                 iface = DCCInterface(j)
+                new_jobs_details.append({
+                    'id': j,
+                    'owner': iface.get_owner(),
+                    'in_progress': 'Ongoing' if iface.get_in_progress() else 'Completed/Failed',
+                    'price': iface.get_price()
+                })
+
                 new_jobs.append(j)
-            except:
-                pass
+            except Exception:
+                traceback.print_exc()
 
         jobs = new_jobs
+        jobs_details = new_jobs_details
 
         time.sleep(5)
 
 @app.route('/')
 def default():
-    return 'You should not be here.'
+    return str(jobs_details)
 
-@app.route('/jobs')
+@app.route('/api/jobs')
 def get_jobs():
     return str(jobs)
 
-@app.route('/addjob', methods=['POST'])
+@app.route('/api/addjob', methods=['POST'])
 def add_job():
     try:
         contract_id = request.data.decode('utf-8')
