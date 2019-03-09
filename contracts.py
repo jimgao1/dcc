7dd880cbc0f20f26cffec4da5ec81fefc04b4af1
Jim Gao <jim@jimgao.tk>
2019-03-09 04:53:51 -0500

    Added contracts stuff

--- contracts.py
+++ contracts.py
@@ -1,14 +1,35 @@
 from web3 import Web3, HTTPProvider
 import json
 
-provider = HTTPProvider('http://10.8.3.1:8545')
-web3 = Web3([provider])
+class DCCInterface:
+    def __init__(self, contract_id):
+        provider = HTTPProvider('http://10.8.3.1:8545')
+        self.web3 = Web3([provider])
 
-print ('latest block', web3.eth.getBlock('latest'))
+        # 0x7eb00cdb92a9e87dab53f64b23936718d5507c35
+        contract_address = Web3.toChecksumAddress(contract_id.lower())
+        contract_abi = json.loads(open('files/abi_compact.json', 'r').read())
 
-contract_address = Web3.toChecksumAddress('0x7eb00cdb92a9e87dab53f64b23936718d5507c35'.lower())
-contract_abi = json.loads(open('files/abi_compact.json', 'r').read())
+        self.contract = self.web3.eth.contract(address=contract_address, abi=contract_abi)
 
-contract = web3.eth.contract(address=contract_address, abi=contract_abi)
+        print("Contract with address {} intialized".format(self.contract.address))
 
-print (contract)
+    def get_owner(self):
+        return self.contract.functions.owner().call()
+
+    def get_in_progress(self):
+        return self.contract.functions.inProgress().call()
+
+    def get_price(self):
+        return self.contract.functions.price().call()
+
+    def get_src_code(self):
+        return self.contract.functions.srcCode().call()
+
+    def submit_result(self, encodedBinary, binHash):
+        pass
+    
+
+# Testing functions
+shit = DCCInterface('0x7eb00cdb92a9e87dab53f64b23936718d5507c35')
+print(shit.get_owner())
---
+++ contracts_src/master.sol
@@ -0,0 +1,93 @@
+pragma solidity ^0.5.5;
+
+contract DCCJobContract {
+    struct Slave {
+        bool exists;
+        uint256 hash;
+        string encodedBinary; // Base64 encoded
+    }
+
+    address payable public owner = msg.sender;
+    bool public inProgress;
+    string public srcCode;
+    uint256 public price;
+    uint32 public numSlavesMax;
+
+    address payable[] slaveAddresses;
+    mapping(address => Slave) slaves;
+
+    event JobCompleted(uint256 hash, uint32[] goodSlaves);
+    event JobFailed();
+
+    constructor(string memory _srcCode, uint32 _numSlavesMax) payable public {
+        require(msg.sender == owner);
+        require(!inProgress);
+
+        srcCode = _srcCode;
+        inProgress = true;
+        price = msg.value;
+        numSlavesMax = _numSlavesMax;
+    }
+
+    function checkCompletion() private {
+        require(slaveAddresses.length == numSlavesMax);
+
+        uint256 maxFreq = 0;
+        uint32[] memory freqs = new uint32[](numSlavesMax);
+        for (uint32 i = 0; i < numSlavesMax; i++) {
+            uint256 shash = slaves[slaveAddresses[i]].hash;
+			uint32 matches = 0;
+            for (uint32 j = 0; j < numSlavesMax; j++) {
+                if (slaves[slaveAddresses[j]].hash == shash) {
+					matches++;
+				}
+            }
+
+			freqs[i] = matches;
+			if (matches > maxFreq) {
+				maxFreq = matches;
+			}
+		}
+
+        uint32 k1 = 0;
+        uint32[] memory goodSlaves = new uint32[](slaveAddresses.length);
+
+        for (uint32 i = 0; i < numSlavesMax; i++) {
+            uint256 shash = slaves[slaveAddresses[i]].hash;
+            if (freqs[shash] == maxFreq) {
+                goodSlaves[k1] = i;
+                k1++;
+            }
+        }
+
+        delete freqs;
+
+        if (10 * k1 < 9 * numSlavesMax) {
+            owner.transfer(price);
+
+            emit JobFailed();
+            inProgress = false;
+        } else {
+            uint256 pricePer = k1 / numSlavesMax;
+            for (uint32 i = 0; i < k1; i++) {
+                slaveAddresses[goodSlaves[i]].transfer(pricePer);
+            }
+            
+            emit JobCompleted(slaves[slaveAddresses[goodSlaves[0]]].hash, goodSlaves);
+            inProgress = false;
+        }
+
+        delete goodSlaves;
+    }
+
+    function submit(uint256 _hash, string calldata _encodedBinary) external {
+        require(!slaves[msg.sender].exists);
+
+        slaveAddresses.push(msg.sender);
+        slaves[msg.sender] = Slave({ exists: true, hash: _hash, encodedBinary: _encodedBinary });
+
+        if (slaveAddresses.length == numSlavesMax) {
+            checkCompletion();
+        }
+    }
+}
