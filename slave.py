b2d0a84c0cc4cc6e50e94df1a756be4c453fc0b3
Jim Gao <jim@jimgao.tk>
2019-03-09 08:54:00 -0500

    More stuff

--- slave.py
+++ slave.py
@@ -7,7 +7,15 @@ import json
 import ipfsapi
 import hashlib
 
+private_key = 'da8328e960737b1a47a7f6a8bc6575809c5c9c5cbbb9bc11c24f58024b018620'
+w3 = Web3([HTTPProvider('http://10.8.3.1:8545')])
+acct = w3.eth.account.privateKeyToAccount(private_key)
+w3.eth.defaultAccount = acct.address
+
 while True:
+    # Sleep
+    time.sleep(5)
+
     # Get jobs
     r = requests.get('http://localhost:5000/api/jobs')
     lst = json.loads(r.text.replace('\'', '"'))
@@ -17,7 +25,6 @@ while True:
 
     # Get first available job
     contract_addr = lst[0]
-    w3 = Web3([HTTPProvider('http://10.8.3.1:8545')])
     iface = DCCInterface(w3, contract_addr)
 
     print (iface)
@@ -46,5 +53,3 @@ while True:
     
     iface.submit_result(post_ipfs_hash, post_file_hash)
 
-    # Sleep
-    time.sleep(5)
